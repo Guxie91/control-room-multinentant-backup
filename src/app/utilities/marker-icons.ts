326c08f13b4d3dea436cccfa4f2335d475c785dc
Guglielmo Bellesi <guglielmo.bellesi@alten.it>
2022-03-21 16:19:46 +0100

    popup upgrade css

--- src/app/utilities/marker-icons.ts
+++ src/app/utilities/marker-icons.ts
@@ -2,28 +2,28 @@ import * as L from "leaflet";
 
 export const RoadworksIcon = L.icon({
   iconUrl: "./assets/img/OrangeRoadworks.png",
-  iconSize: [34, 30], // size of the icon
+  iconSize: [33, 29], // size of the icon
   iconAnchor: [17, 30], // point of the icon which will correspond to marker's location
   popupAnchor: [0, -30], // point from which the popup should open relative to the iconAnchor
 });
 
 export const InfoIcon = L.icon({
   iconUrl: "./assets/img/OrangeInfo.png",
-  iconSize: [34, 30], // size of the icon
+  iconSize: [33, 29], // size of the icon
   iconAnchor: [17, 30], // point of the icon which will correspond to marker's location
   popupAnchor: [0, -30], // point from which the popup should open relative to the iconAnchor
 });
 
 export const WeatherIcon = L.icon({
   iconUrl: "./assets/img/OrangeWeather.png",
-  iconSize: [36, 30], // size of the icon
+  iconSize: [33, 29], // size of the icon
   iconAnchor: [18, 30], // point of the icon which will correspond to marker's location
   popupAnchor: [0, -30], // point from which the popup should open relative to the iconAnchor
 });
 
 export const TrafficIcon = L.icon({
   iconUrl: "./assets/img/OrangeTraffic.png",
-  iconSize: [33, 30], // size of the icon
+  iconSize: [33, 29], // size of the icon
   iconAnchor: [22, 30], // point of the icon which will correspond to marker's location
   popupAnchor: [0, -30], // point from which the popup should open relative to the iconAnchor
 });
