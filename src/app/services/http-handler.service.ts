841ae05f6e64fc500038a05040d80ac2e9058912
Guglielmo Bellesi <guglielmo.bellesi@alten.it>
2022-05-20 12:01:05 +0200

    removed unused import

--- src/app/services/http-handler.service.ts
+++ src/app/services/http-handler.service.ts
@@ -3,7 +3,6 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Tenant } from "../models/tenant.model";
 import { MqttSettings } from "../models/mqtt-settings";
-import { take } from "rxjs/operators";
 
 @Injectable({
   providedIn: "root",
